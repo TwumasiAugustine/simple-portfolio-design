d3065b91b8322befe34a2ceb403684310d61c959
Augustine Twumasi <twumasiaugustine1997@gmail.com>
2025-04-06 18:04:08 +0000

    Add configuration for spell checking and update sitemap; refactor toggle to mobile menu

--- scripts.js
+++ scripts.js
@@ -1,36 +1,17 @@
-// Dark/Light mode toggle
-const themeToggle = document.getElementById('theme-toggle');
-const themeIcon = document.getElementById('theme-icon');
-const themePath = document.getElementById('theme-path');
-const htmlElement = document.documentElement;
+// MobileMenu Toggle
+const menu_toggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('mobile-menu');
 
-console.log('Theme Toggle Element:', themeToggle);
-console.log('Theme Icon Element:', themeIcon);
-console.log('Theme Path Element:', themePath);
-console.log('Current Theme:', localStorage.getItem('theme'));
-console.log('HTML Classes:', htmlElement.classList);
+const toggleMenu = () => {
+    if(menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+    menu_toggle.classList.toggle('active')
+};
 
-// Check if theme is already set in localStorage
-const savedTheme = localStorage.getItem('theme') || 'dark';
-htmlElement.classList.add(savedTheme);
-
-// Update the icon based on the current theme
-updateThemeIcon(savedTheme === 'dark');
-
-themeToggle.addEventListener('click', () => {
-    const isDark = htmlElement.classList.toggle('dark');
-    localStorage.setItem('theme', isDark ? 'dark' : 'light');
-    updateThemeIcon(isDark);
-    console.log('Theme toggled:', isDark ? 'dark' : 'light');
-    console.log('Body classes:', document.body.classList);
-});
-
-function updateThemeIcon(isDark) {
-    themePath.setAttribute('d', isDark ? 
-        'M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z' :
-        'M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
-    );
-}
+menu_toggle.addEventListener('click', toggleMenu);
 
 // Scroll animations
 const observer = new IntersectionObserver((entries) => {
@@ -70,13 +51,8 @@ document.querySelectorAll('.slide-right').forEach(el => {
     slideRightObserver.observe(el);
 });
 
-// Check if Tailwind dark mode classes are working
-const testElement = document.createElement('div');
-testElement.className = 'bg-white dark:bg-gray-900 p-4';
-testElement.textContent = 'Test Element';
-document.body.appendChild(testElement);
 
-console.log('Test Element Classes:', testElement.classList);
+// year on footer
+const year = new Date().getFullYear();
+document.getElementById('year').textContent = year;
 
-document.documentElement.classList.toggle('dark')
-localStorage.getItem('theme') 
